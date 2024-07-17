e235a63a200f3282ab48482e41707e6e460ec913
yurarud <yurarud@gmail.com>
2024-07-17 10:12:59 +0300

    git config --global user.email yurarud@gmail.com

--- src/main/java/com/example/faculty/model/DescriptionItem.java
+++ src/main/java/com/example/faculty/model/DescriptionItem.java
@@ -1,38 +1,19 @@
 package com.example.faculty.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "description_items")
 public class DescriptionItem {
+    // Getters and setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer pageId;
     private String text;
 
-    // Getters and setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPageId() {
-        return pageId;
-    }
-
-    public void setPageId(Integer pageId) {
-        this.pageId = pageId;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
